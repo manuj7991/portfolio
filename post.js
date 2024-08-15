b35b6d2eca9ad15b30d9b3fed20d57cf567917ca
Manuj Pandey <manujk320@gmail.com>
2024-08-15 17:45:04 +0530

    Deleted posts

--- post.js
+++
@@ -1,6 +0,0 @@
-(function () {
-    //document.getElementById("blog-menu").classList.remove("blog-active");
-    //[...document.querySelectorAll(".blog-posts")].forEach(item => {
-    //});
-    //document.getElementById("blog-menu").classList.add("blog-inactive");
-})();
